13cc5d657696f12742deb881dca168bcb98804e6
Matthieu Baty <matthieu.baty@inria.fr>
2022-04-02 16:22:38 +0200

    cleanup, add basic client scene

---
+++ src/Client/Client.gd
@@ -0,0 +1,22 @@
+extends Node
+
+var money = 30
+onready var in_store_timer = $in_store_timer
+var target
+var in_cart = []
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+func enters_range(var product):
+	if (target )
+		
+	pass
+
+func is_interested(var product):
+	if (shopping_list.has())
+
+func 
--- src/Player/Player.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _ready():
-	# Waits for Game.gd to run randomize()
-	yield(get_tree(), "idle_frame")
-	$SoundFx/SpawnSound.play_sound()
-
-#func _process(delta):
-#	pass

48a27cee624bc2cd68873993ddef35087c8d8be1
Nicolas BUSSER <nicolas.busser67@gmail.com>
2022-04-03 14:03:49 +0200

    add drag for build or delete

--- src/Game/Game.gd
+++ src/Game/Game.gd
@@ -2,23 +2,51 @@ extends Node
 
 onready var map = $Map
 
+var drag_build = false
+var drag_destroy = false
+
+func not_dragging():
+	return !drag_build and !drag_destroy
+
 func _ready():
 	randomize()
+
+func build():
+	if GameState.selected_tool == GameState.Tool.AISLE:
+		map.summon_aisle()
+	elif GameState.selected_tool == GameState.Tool.PRODUCT:
+		assert(GameState.selected_product != null)
+		map.summon_product(GameState.selected_product)
 	
+func destroy():
+	map.remove_tile()
+
 func _input(event):
+	if GameState.game_mode != GameState.GameMode.DESIGN:
+		return
+
+	if event is InputEventMouseButton:
+		if event.pressed:
+			if event.button_index == BUTTON_LEFT and not_dragging():
+				drag_build = true
+				build()
+			elif event.button_index == BUTTON_RIGHT and not_dragging():
+				drag_destroy = true
+				destroy()
+		else:
+			if event.button_index == BUTTON_LEFT:
+				drag_build = false
+			elif event.button_index == BUTTON_RIGHT:
+				drag_destroy = false
+
 	if (
-		event is InputEventMouseButton
-		and event.pressed
+		event is InputEventMouseMotion
 		and GameState.game_mode == GameState.GameMode.DESIGN
 	):
-		if event.button_index == BUTTON_LEFT:
-			if GameState.selected_tool == GameState.Tool.AISLE:
-				map.summon_aisle()
-			elif GameState.selected_tool == GameState.Tool.PRODUCT:
-				assert(GameState.selected_product != null)
-				map.summon_product(GameState.selected_product)
-		elif event.button_index == BUTTON_RIGHT:
-			map.remove_tile()
+		if drag_build:
+			build()
+		elif drag_destroy:
+			destroy()
 
 func _on_ActionUI_begin_simulation():
 	GameState.game_mode = GameState.GameMode.SIMULATION

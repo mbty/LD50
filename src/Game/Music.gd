d0f3e6c7b26a4c8967e12fd63f024bb9a2fe9217
ValerianOS <valerian.coulon@offshadow.com>
2022-04-03 18:04:35 +0200

    Added player

--- src/Game/Music.gd
+++ src/Game/Music.gd
@@ -1,14 +1,96 @@
 extends Node
 
+const INTRO = "intro"
+const LOOP = "loop"
+const END = "end"
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const MENU = "musicMenu"
+
+const MUSIC_ATTENUATION_STOP = -20
+
+onready var currentIntroMusic = null
+onready var currentLoopMusic = $Music1
+onready var currentEndMusic = null
+onready var nextIntroMusic = null
+onready var nextLoopMusic = null
+onready var nextEndMusic = null
+onready var currentMusic = null
+onready var menuMusic = null
+
+onready var currentMusicType = LOOP
+onready var currentMusicName = MENU
+onready var nextMusicName = null
+onready var musicToAttenuate = null
+onready var musicAttenuationStart = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Music1.play()
+	currentMusic = $Music1
+	currentMusic.play()
+	
+func _on_CurrentMusic_finished():
+	match currentMusicType:
+		INTRO:
+			if (currentLoopMusic != null && currentLoopMusic.stream.has_loop()):
+				changeMusic(currentLoopMusic)
+				currentMusicType = LOOP
+			elif (currentEndMusic != null):
+				changeMusic(currentEndMusic)
+				currentMusicType = END
+			else:
+				changeMusicToNext()
+		LOOP:
+			currentMusic.stream.set_loop(true)
+			if (currentEndMusic != null):
+				changeMusic(currentEndMusic)
+				currentMusicType = END
+			else:
+				changeMusicToNext()
+		END:
+			changeMusicToNext()
+	
+	
+func changeMusicToNext():
+	musicToAttenuate = null
+	if (nextIntroMusic != null):
+		changeMusic(nextIntroMusic)
+		updateMusicToNext()
+		currentMusicType = INTRO
+	elif (nextLoopMusic):
+		changeMusic(nextLoopMusic)
+		updateMusicToNext()
+		currentMusicType = LOOP
+	elif (nextEndMusic):
+		changeMusic(nextEndMusic)
+		updateMusicToNext()
+		currentMusicType = END
+	else:
+		nextIntroMusic = currentIntroMusic
+		nextLoopMusic = currentLoopMusic
+		nextEndMusic = currentEndMusic
+		changeMusicToNext()
+		
+func updateMusicToNext():
+	currentMusicName = nextMusicName
+	currentIntroMusic = nextIntroMusic
+	currentLoopMusic = nextLoopMusic
+	currentEndMusic = nextEndMusic
+	nextMusicName = null
+
+func menuEnter():
+	currentMusic.stream.set_stream_paused(true)
+	menuMusic.play()
+
+func menuExit():
+	menuMusic.stop()
+	currentMusic.stream.set_stream_paused(false)
+	
+func changeMusic(next):
+	currentMusic.disconnect("finished", self, "_on_CurrentMusic_finished")
+	currentMusic = next
+	currentMusic.play()
+	currentMusic.connect("finished", self, "_on_CurrentMusic_finished")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

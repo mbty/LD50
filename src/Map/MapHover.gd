04b04a13d6964054329e4d819a15a3576b53e6c0
Antoine Brûlé <antoine.brule@netc.eu>
2022-04-03 10:59:06 +0200

    add map hover transparency

--- src/Map/MapHover.gd
+++ src/Map/MapHover.gd
@@ -1,12 +1,15 @@
 extends Control
 
 onready var product_sprite = $ProductSprite
+onready var frame = $Frame
 
 export var show_product = false
 
 func _process(delta):
-	if (GameState.selected_product != null) && self.show_product:
+	if GameState.selected_product != null && self.show_product:
+		frame.modulate.a = 1
 		product_sprite.texture = GameState.selected_product.get_texture()
 		product_sprite.show()
 	else:
 		product_sprite.hide()
+		frame.modulate.a = 0.3

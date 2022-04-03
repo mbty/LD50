311ab0c8da734a45d6ec3e9d81ff7bb1d7137dc9
Nicolas BUSSER <nicolas.busser67@gmail.com>
2022-04-03 10:12:27 +0200

    add prototypal simuation mode

--- src/Game/Game.gd
+++ src/Game/Game.gd
@@ -3,13 +3,27 @@ extends Node
 signal summon_product
 
 onready var map = $Map
+var building_mode = true
 
 func _ready():
 	randomize()
-	map.create_client($Products.get_children())
 	
 func _input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_LEFT and event.pressed and GameState.selected_product != null:
-			emit_signal("summon_product", GameState.selected_product)
+	if (
+		event is InputEventMouseButton
+		and event.button_index == BUTTON_LEFT
+		and event.pressed
+		and GameState.selected_product != null
+		and self.building_mode
+	):
+		emit_signal("summon_product", GameState.selected_product)
 
+func _on_ActionUI_begin_simulation():
+	building_mode = false
+	$UI/ActionUI.hide()
+	$SimulationModeTimer.start()
+	map.create_client($Products.get_children())
+
+func _on_SimulationModeTimer_timeout():
+	building_mode = true
+	$UI/ActionUI.show()
--- src/UI/ActionUI.gd
+++ src/UI/ActionUI.gd
@@ -1,5 +1,7 @@
 extends Control
 
+signal begin_simulation
+
 onready var product_list = $VBoxContainer/HBoxContainer/VBoxContainer/ProductList
 onready var product_ui_scene = preload("res://src/UI/ProductUI.tscn")
 
@@ -12,3 +14,6 @@ func _on_Products_new_product(product):
 	product_list.add_child(product_instance)
 	product_instance.init(product)
 	product_instance.connect("product_selected", GameState, "on_product_selected")
+
+func _on_PlayBtn_button_down():
+	emit_signal("begin_simulation")

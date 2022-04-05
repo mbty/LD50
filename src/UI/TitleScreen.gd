89fdf721ac8571398bd538c3c8f914ddb5ddff34
Nicolas BUSSER <nicolas.busser67@gmail.com>
2022-04-05 03:13:34 +0200

    add tuto

--- src/UI/TitleScreen.gd
+++ src/UI/TitleScreen.gd
@@ -2,13 +2,25 @@ extends Control
 
 onready var credits = $CanvasLayer/Credits
 onready var credits_button = $Node2D/VBoxContainer/CenterContainer2/VBoxContainer/CreditsButton
+onready var btn = $Node2D/VBoxContainer/CenterContainer2/VBoxContainer/PlayButton
+onready var tuto1 = $Tuto1
+onready var tuto2 = $Tuto2
 
 func _ready():
 	credits.visible = false
 
-func _on_PlayButton_pressed():
-	if get_tree().change_scene("res://src/Game/Game.tscn") != OK:
-		printerr('error')
-
 func _on_CreditsButton_pressed():
 	credits.visible = credits_button.pressed
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		if tuto1.visible:
+			tuto1.hide()
+			tuto2.show()
+		elif tuto2.visible:
+			if get_tree().change_scene("res://src/Game/Game.tscn") != OK:
+				printerr('error')
+
+func _on_PlayButton_pressed():
+	btn.hide()
+	tuto1.show()

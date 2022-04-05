6fcae7fb4c0f0907b3e5b185f4bc00b41cb96e1e
Antoine Brûlé <antoine.brule@netc.eu>
2022-04-05 02:46:44 +0200

    add credits on title screen

--- src/UI/TitleScreen.gd
+++ src/UI/TitleScreen.gd
@@ -1,5 +1,14 @@
 extends Control
 
+onready var credits = $Credits
+onready var credits_button = $Node2D/VBoxContainer/CenterContainer2/VBoxContainer/CreditsButton
+
+func _ready():
+	credits.visible = false
+
 func _on_PlayButton_pressed():
 	if get_tree().change_scene("res://src/Game/Game.tscn") != OK:
 		printerr('error')
+
+func _on_CreditsButton_pressed():
+	credits.visible = credits_button.pressed
